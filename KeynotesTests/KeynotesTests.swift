3c880c4f132b032b425570e3dd6f3617fd2530ee
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-08 22:31:28 +0900

    Initial Commit

---
+++ KeynotesTests/KeynotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeynotesTests.swift
+//  KeynotesTests
+//
+//  Created by Thomas Süssli on 08.02.2026.
+//
+
+import Testing
+@testable import Keynotes
+
+struct KeynotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
